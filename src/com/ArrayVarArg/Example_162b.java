a4123bf09b862086af2cd7c1fa553ce0101b19e3
Usha Manduri <ushamanduri95jz4u@gmail.com>
2022-09-14 11:00:15 -0400

    Committing Example_162a, 162b

---
+++ src/com/ArrayVarArg/Example_162b.java
@@ -0,0 +1,10 @@
+package com.ArrayVarArg;
+
+public class Example_162b {
+	int x = 10;
+	int y = 20;
+	
+	void m1() {
+		x = 5;
+	}
+}
---
+++ src/com/inter/Square.java
@@ -0,0 +1,20 @@
+package com.inter;
+
+public class Square implements Shape{
+
+	@Override
+	public void findArea() {
+		// TODO Auto-generated method stub
+		System.out.println("Area of square is s^2");
+		
+	}
+
+	@Override
+	public void findPerimeter() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Perimetr of square is 4B");
+		
+	}
+
+}
