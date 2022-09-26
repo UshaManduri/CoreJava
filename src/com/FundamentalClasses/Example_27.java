5119dc5371fa0917fbd3d3ecf9f8f767f96bc55c
Usha Manduri <ushamanduri95jz4u@gmail.com>
2022-09-26 17:04:54 -0400

    hashCode() demo

---
+++ src/com/FundamentalClasses/Example_27.java
@@ -0,0 +1,7 @@
+package com.FundamentalClasses;
+
+public class Example_27 {
+	int x = 10;
+	int y = 20;
+	int z = 40;
+}
---
+++ src/com/FundamentalClasses/HashCodeDemo_27.java
@@ -0,0 +1,13 @@
+package com.FundamentalClasses;
+
+public class HashCodeDemo_27 {
+	public static void main(String[] args) {
+		Example_27 e1 = new Example_27();
+		Example_27 e2 = new Example_27();
+		Example_27 e3 = new Example_27();
+		
+		System.out.println(e1.hashCode());
+		System.out.println(e2.hashCode());
+		System.out.println(e3.hashCode());
+	}
+}
