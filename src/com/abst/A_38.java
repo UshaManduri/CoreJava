0512d23b3a5728ab83d951a70ea6899e5daba8f9
Usha Manduri <ushamanduri95jz4u@gmail.com>
2022-09-28 18:17:14 -0400

    toString()

---
+++ src/com/abst/A_38.java
@@ -0,0 +1,11 @@
+package com.abst;
+
+public class A_38 {
+	int x;
+	A_38(int x){
+		this.x = x;
+	}
+	public String toString() {
+		return""+x;
+	}
+}
---
+++ src/com/abst/Test_38.java
@@ -0,0 +1,11 @@
+package com.abst;
+
+public class Test_38 {
+	public static void main(String[] args) {
+		A_38 a1 = new A_38(5);
+		A_38 a2 = new A_38(6);
+		
+		System.out.println(a1);
+		System.out.println(a2 );
+	}
+}
